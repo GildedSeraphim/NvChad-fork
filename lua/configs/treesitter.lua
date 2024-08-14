d33dea7d5c8df613ca3c456ad87117e1e7659d2b
GildedSeraphim <nathanishaaz@gmail.com>
2024-08-14 11:08:35 -0500

    initial commit

---
+++ lua/configs/treesitter.lua
@@ -0,0 +1,34 @@
+local options = {
+	ensure_installed = {
+		"bash",
+		"c",
+		"cmake",
+		"cpp",
+		"fish",
+		"glsl",
+		-- "go",
+		-- "gomod",
+		-- "gosum",
+		-- "gotmpl",
+		-- "gowork",
+		"lua",
+		"luadoc",
+		"make",
+		"markdown",
+		"printf",
+		-- "python",
+		"toml",
+		"vim",
+		"vimdoc",
+		"yaml",
+	},
+
+	highlight = {
+		enable = true,
+		use_languagetree = true,
+	},
+
+	indent = { enable = true },
+}
+
+require("nvim-treesitter.configs").setup(options)
